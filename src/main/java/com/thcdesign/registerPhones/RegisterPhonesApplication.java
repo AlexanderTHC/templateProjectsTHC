fd5c9b81775b7de4347b88519fe2bdfd6d8c2b9c
z4x <sosaalexander.joa@gmail.com>
2023-02-06 15:14:51 -0300

    TEMPLATE DE PROYECTOS SPRING BOOT V2.5.5 CON JAVA VERSION 8 (1.8) BY THC. ESQUELETO PARA UTILIZAR EN PROYECTOS DE SPRING BOOT

---
+++ src/main/java/com/thcdesign/registerPhones/RegisterPhonesApplication.java
@@ -0,0 +1,13 @@
+package com.thcdesign.registerPhones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterPhonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterPhonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thcdesign/registerPhones/RegisterPhonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thcdesign.registerPhones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterPhonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
